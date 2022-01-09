1643576185d7e0d9c9bef856b2b881e0cb514532
pabloL0pez <pablo.lopez3895@gmail.com>
2022-01-09 18:50:25 -0300

    fix pipe

--- src/app/modules/comments/pipes/highlight-user/highlight-user.pipe.ts
+++ src/app/modules/comments/pipes/highlight-user/highlight-user.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-const userRegExp: RegExp = new RegExp(/@{1}[a-z]+/i);
+const userRegExp: RegExp = new RegExp(/@{1}[a-z]+/ig);
 
 @Pipe({
     name: 'highlightUser',
@@ -16,7 +16,7 @@ export class HighlightUserPipe implements PipeTransform {
         return this.domSanitizer.bypassSecurityTrustHtml(
             value.split(' ').map(text => text.replace(userRegExp,
                     `<span style='color: var(--moderate-blue); font-weight: var(--weight-bold); font-size: inherit;'>
-                        ${value.match(userRegExp)}
+                        ${value.match(userRegExp)?.find(item => item == text.substring(0, item.length))}
                     </span>`
                 )
             ).join(' ')
